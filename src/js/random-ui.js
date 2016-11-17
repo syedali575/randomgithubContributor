52fb31db13a7ae3b85fbc4a029b633c798660c13
Syed Ali <sali3@cox.net>
2016-11-17 15:41:23 -0500

    Able to pick a random object out of 30, and able to access commit_urls of the object

--- src/js/random-ui.js
+++ src/js/random-ui.js
@@ -16,9 +16,14 @@
 
 
       window.random.searchRepo(api, query)
-      
+
       .done(function handleSuccess(data){
         console.log("It worked", data);
+
+        var pickMe = data.items;
+        var randomPick = pickMe[Math.floor(Math.random() * 30)];
+        console.log(randomPick);
+        console.log(randomPick.commits_url);
         // return promise
       })
       // add another done
